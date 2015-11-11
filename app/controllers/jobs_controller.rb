cbfa162c43504afa0cb05f063b32ed539a52d556
Jeremy <jgertz26@gmail.com>
2015-11-11 12:59:27 -0500

    new/create jobs works

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,4 +8,23 @@ class JobsController < ApplicationController
   def show
     @job = get_json(request.path)
   end
+
+  def new
+  end
+
+  def create
+    uri = URI("http://localhost:3000/jobs")
+    res = Net::HTTP.post_form(uri, job_params)
+    binding.pry
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    output = params.permit(
+      :poster, :company, :city, :state, :title, :body, :keywords
+    )
+    output.merge(posted: Time.now.to_i)
+  end
 end
